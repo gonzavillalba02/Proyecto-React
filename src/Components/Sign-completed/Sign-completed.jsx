97becf914b5a7e91aabee21e028f3a1c6885508b
Gonza <gonzavillalba123@gmail.com>
2023-10-10 23:19:48 -0300

    Agregando pantalla de registro completado

--- src/Components/Sign-completed/Sign-completed.jsx
+++ src/Components/Sign-completed/Sign-completed.jsx
@@ -1,8 +1,45 @@
 import React from "react";
+import "./Sign-completed.css"
+import completed from "../../assets/img/sign-completed-1.png"
+import instagram from "../../assets/img/instagram-1.png"
+import facebook from "../../assets/img/facebook-1.png"
+import twitter from "../../assets/img/twitter-1.png"
 
-const Sign_completed = () => {
+const Sign_completed = ({HandleSign}) => {
     return(
-        <div>hola</div>
+        <div className="sign-completed-container">
+            <h3>Registration completed !</h3>
+            <img src={completed} alt="Registro completado" />
+            <button 
+            className="sign-completed-button"
+            onClick={()=>{HandleSign(0)}}
+            >
+                Log in
+            </button>
+            <div className="sign-redes-completed">
+                <a href="https://instagram.com" target="_blank">
+                    <img 
+                    id="sign-redes-div-instagram" 
+                    src={instagram} 
+                    alt="Instagram" 
+                    />
+                </a>
+                <a href="https://facebook.com" target="_blank">
+                    <img 
+                    id="sign-redes-div-facebook" 
+                    src={facebook} 
+                    alt="Facebook" 
+                    />
+                </a>
+                <a href="https://twitter.com" target="_blank">
+                    <img 
+                    id="sign-redes-div-twitter" 
+                    src={twitter} 
+                    alt="Twitter" 
+                    />
+                </a>
+            </div>
+        </div>
     )
 }
 
